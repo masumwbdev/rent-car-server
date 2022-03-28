cc2dac54465c34dfccae1a2540990e40d79a270a
masumwbdev <masumwbd@gmail.com>
2022-03-28 17:50:26 +0600

    rent car server, sent service api

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const ObjectId = require('mongodb').ObjectId;
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qr0wp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("rent-car");
+        const servicesCollection = database.collection("services");
+
+        // services get
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const service = await cursor.toArray();
+            res.send(service);
+        })
+
+        // get single service
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query ={_id: ObjectId(id)};
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
